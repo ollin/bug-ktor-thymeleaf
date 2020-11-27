ca2b6d383d304d3b64f6d7df924429a91885ad93
ollin <oliver.nautsch@gmail.com>
2020-11-27 14:40:24 +0100

    add source from from start.ktor.io

---
+++ src/Application.kt
@@ -0,0 +1,37 @@
+package bug
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.thymeleaf.Thymeleaf
+import io.ktor.thymeleaf.ThymeleafContent
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Thymeleaf) {
+        setTemplateResolver(ClassLoaderTemplateResolver().apply {
+            prefix = "templates/thymeleaf/"
+            suffix = ".html"
+            characterEncoding = "utf-8"
+        })
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-thymeleaf") {
+            call.respond(ThymeleafContent("index", mapOf("user" to ThymeleafUser(1, "user1"))))
+        }
+    }
+}
+
+data class ThymeleafUser(val id: Int, val name: String)
+
